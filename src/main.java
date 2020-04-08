6d6eb56e3dc97ac90a133d5fc37296b781927cd1
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-04-08 09:01:43 +0200

    main

--- src/main.java
+++ src/main.java
@@ -2,6 +2,19 @@ public class main {
 
     public static void main(String[] args) {
 
+        //création des 5 objets de types villes
+        Ville dijon = new Ville("Dijon", 40.41, 375831);
+        Ville quetigny = new Ville("Quetigny", 8.19,9690);
+        Ville beaune = new Ville("Beaune", 31.3,52741);
+        Ville macon = new Ville("Mâcon", 27.0,100172);
+        Ville chalon = new Ville("Chalon-sur-Saône", 15.22,133557);
+
+        //création des 2 départements
+
+        //ajout des villes aux départements
+
+        //Affichage des villes
+        System.out.println(dijon);
 
 
     }
