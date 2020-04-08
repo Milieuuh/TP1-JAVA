ddf39572d3ccd9be6623051824c54198c64e6b6e
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-04-08 08:47:19 +0200

    Update Ville.java

--- src/Ville.java
+++ src/Ville.java
@@ -4,7 +4,46 @@ public class Ville
     private double supperfie;
     private  int population;
 
+    public Ville()
+    {
+        nom="inconnu";
+        supperfie=0;
+        population=0;
+    }
+
+    public Ville(String nom, double superficie, int pop)
+    {
+        this.nom=nom;
+        this.supperfie = superficie;
+        this.population=pop;
+    }
 
 
+    /////////////////////////////////GETTER
+    public String getNom() {
+        return nom;
+    }
+
+    public double getSupperfie() {
+        return supperfie;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    ///////////////////////////////SETTER
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setSupperfie(double supperfie) {
+        this.supperfie = supperfie;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 
 }
