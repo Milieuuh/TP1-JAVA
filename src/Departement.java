19c10ee474915abe91570df34d1bdd5fdbea5842
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-04-08 08:57:06 +0200

    Update Departement.java

--- src/Departement.java
+++ src/Departement.java
@@ -3,12 +3,41 @@ public class Departement {
     private Ville tabVilles[];
     private int numero;
     private String nom;
-    private int nbVillesSaisies;
+    private int nbVillesSaisies=0;
 
     //constructor
-    public Departement(int nbVilles)
+    public Departement()
     {
-        tabVilles=new Ville[nbVilles];
+
+    }
+    public Departement(int nbVilles,String nom, int numero)
+    {
+        this.tabVilles=new Ville[nbVilles];
+        this.nom=nom;
+        this.numero=numero;
+    }
+
+    public void ajouterVille(Ville ville)
+    {
+        if(nbVillesSaisies< tabVilles.length)
+        {
+            tabVilles[nbVillesSaisies]=ville;
+            nbVillesSaisies++;
+
+        }
+        else
+        {
+            System.out.println("Attention ! VOus ne pouvez plus rajouter de ville.")
+        }
+    }
+
+    public void toString()
+    {
+        System.out.println("Ville du département "+nom+" :");
+        for(int i=0;i<tabVilles.length;i++)
+        {
+            tabVilles[i]
+        }
     }
 }
 
