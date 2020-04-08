ea731cf0230c1763e5a4f8db644fe46bfd21c968
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-04-08 08:47:57 +0200

    departement

--- src/Departement.java
+++ src/Departement.java
@@ -1,5 +1,15 @@
 public class Departement {
-    
+    //attributs
+    private Ville tabVilles[];
+    private int numero;
+    private String nom;
+    private int nbVillesSaisies;
+
+    //constructor
+    public Departement(int nbVilles)
+    {
+        tabVilles=new Ville[nbVilles];
+    }
 }
 
 
